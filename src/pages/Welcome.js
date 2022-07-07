03b47264f5965e5897a749de5fd6bbe1d1973180
hari-p02 <hari.patchigolla@uconn.edu>
2022-07-07 11:22:15 -0400

    got rid of scroll effects

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography, makeStyles } from '@material-ui/core'
 import WelcomePgContent from './WelcomePgContent';
 import WelcomePGUCONN from './WelcomePGUCONN';
-import { ScrollContainer, ScrollPage, Animator, batch, Sticky, Fade, MoveOut, Zoom, StickyIn, FadeIn, ZoomIn } from 'react-scroll-motion';
+// import { ScrollContainer, ScrollPage, Animator, batch, Sticky, Fade, MoveOut, Zoom, StickyIn, FadeIn, ZoomIn } from 'react-scroll-motion';
 // import {teal} from '@mui/material/colors';
 // import { spacing } from '@mui/system';
 
@@ -28,50 +28,37 @@ const useStyles = makeStyles({
     color: "#A0E7E5"
   }
 });
-const ZoomInScrollOut = batch(FadeIn(), ZoomIn());
 
 const Welcome = () => {
   const classes = useStyles();
   return (
-    <ScrollContainer>
-      <ScrollPage page={0}>
-        <Animator animation={Fade()}>
-          <div className='bkimg'>
-            <div className='welcome-header'>
-                <Typography 
-                  variant="h2"
-                  className={classes.root} 
-                  >
-                  Welcome to Our 
-                </Typography>
-                <Typography 
-                  variant="h2" 
-                  className={classes.root} 
-                  >
-                  Bioinformatics Course!
-                </Typography>
-                <Typography 
-                  variant="h6" 
-                  className={classes.root} 
-                  >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                </Typography>
-            </div>
-          </div>
-        </Animator>
-      </ScrollPage>
-      <ScrollPage page={1}>
-        <Animator animation={ZoomInScrollOut}>
-          <WelcomePgContent />
-        </Animator>
-      </ScrollPage>
-      <ScrollPage page={2}>
-        <Animator animation={FadeIn()}>
-          <WelcomePGUCONN />
-        </Animator>
-      </ScrollPage>
-    </ScrollContainer>
+    <div>
+      <div className='bkimg'>
+        <div className='welcome-header'>
+            <Typography 
+              variant="h2"
+              className={classes.root} 
+              >
+              Welcome to Our 
+            </Typography>
+            <Typography 
+              variant="h2" 
+              className={classes.root} 
+              >
+              Bioinformatics Course!
+            </Typography>
+            <Typography 
+              variant="h6" 
+              className={classes.root} 
+              >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            </Typography>
+        </div>
+      </div>
+      <WelcomePgContent />
+      <WelcomePGUCONN />
+    </div>
   )
 }
 

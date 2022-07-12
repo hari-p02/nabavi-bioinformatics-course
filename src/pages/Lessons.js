065df2b906426f490e9550f1025a426c90d6c0ad
hari-p02 <hari.patchigolla@uconn.edu>
2022-07-11 20:41:42 -0400

    added header to lessons

--- src/pages/Lessons.js
+++ src/pages/Lessons.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Container, Grid} from '@material-ui/core'
+import {Typography,  makeStyles } from '@material-ui/core'
 import CourseCard from './CourseCard'
-
+const useStyles = makeStyles({
+  root: {
+    color: 'white',
+  },
+  something: {
+    color: "#A0E7E5"
+  }
+});
 const Lessons = ({ courses }) => {
-
+  const classes = useStyles();
   return (
     <div>
-      <div className='lessonbkimg'></div>
+      <div className='lessonbkimg'>
+        <div className='lessonheader'>
+          <Typography
+          variant="h2"
+          className={classes.root}>
+            Modules!
+          </Typography>
+        </div>
+      </div>
       <div className='grid-container-2'>
               {courses.map(course => (
                       <CourseCard course={course} sha={11}/>
